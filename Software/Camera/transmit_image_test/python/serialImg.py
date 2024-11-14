97966b035646fd8c875884b6ed196e2b020a755e
Robin Nicole <robin.nicole@epfl.ch>
2024-11-14 14:29:23 +0100

    Finalised the structure of the code

--- Software/Camera/transmit_image_test/python/serialImg.py
+++ Software/Camera/transmit_image_test/python/serialImg.py
@@ -2,28 +2,6 @@
 
 # Source : https://stackoverflow.com/questions/62031686/reading-a-serial-signal-from-a-usb-port-with-python
 import serial;
-import io;
-import time;
-import os;
-
-if __name__ == '__main__' :
-    end = False
-    while not end:
-        try :
-            # configure the serial connections (the parameters differs on the device you are connecting to)
-            s = serial.Serial(port='/dev/cu.usbserial-A5XK3RJT Serial Port (USB)', baudrate=115200, timeout=1,
-                           xonxoff=False, rtscts=False, dsrdtr=True)
-
-            data = s.readline()
-            f = open("img.jpg", "w")
-            print(data)
-            f.write(data)
-            end = True
-            f.close()
-            s.close()
-        except :
-            pass
-    pass
 
 class SerialImg(serial.Serial):
     xonxoff = False
@@ -37,3 +15,23 @@ class SerialImg(serial.Serial):
         self.port     = port,
         self.baudrate = baudrate
         self.timeout  = timeout
+
+    def getImg(self):
+        """
+        Returns the current image sent by the device on the serial
+        connection
+        """
+
+        img = b''
+
+        end = False
+        while not end:
+            try:
+                img += self.readline()
+
+                # TODO: Add the right condition to end while loop
+                end = True
+            except:
+                img = None
+            finally:
+                return img
