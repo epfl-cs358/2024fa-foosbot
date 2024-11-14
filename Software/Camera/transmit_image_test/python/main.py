733b6a8835dc5e82cce22230aade4c798b8e05a6
Robin Nicole <robin.nicole@epfl.ch>
2024-11-14 18:42:35 +0100

    Changing code architecture to implement image processing
    The previous code is kept in `serialToFile.py` for debugging purposes

--- Software/Camera/transmit_image_test/python/main.py
+++ Software/Camera/transmit_image_test/python/main.py
@@ -8,18 +8,25 @@ def main():
     port = input("Port ['/dev/ttyUSB0']: ")
     s = SerialImg() if not port else SerialImg(port=port)
 
+    print("Starting serial reception service...\n")
+    print("To kill the service, press <CTRL-C>.")
     try:
-        data = s.getImg()
+        while True:
+            data = s.getImg()
 
-        if data is None:
-            print("Image could not be read.")
-        elif not data:
-            print("Image produced seems to be empty, " +
-                  "I will not write to the file.")
-        else:
-            f = open("img.bmp", "wb")
-            f.write(data)
-            f.close()
+            if data is None:
+                print("Image could not be read.")
+            elif not data:
+                print("Image produced seems to be empty, " +
+                      "I will not write to the file.")
+            else:
+                # TODO: Remove writing to file
+                # f = open("img.bmp", "wb")
+                # f.write(data)
+                # f.close()
+
+                # TODO: Send image to processing for ball detection
+                pass
     except:
         pass
     finally:
