6da4a6f59ddfb515f27fecee015c27440ef2b778
Robin Nicole <robin.nicole@epfl.ch>
2024-11-20 10:10:33 +0100

    Python detection with openCV works but slowly (#21)

--- Software/Camera/transmit_image_test/python-wifi/main.py
+++ Software/Camera/transmit_image_test/python-wifi/main.py
@@ -1,3 +1,40 @@
 #!/usr/bin/env python3
 
+import sys
+import urllib.request
+import cv2
+import numpy as np
 
+def main():
+    """TODO: Docstring for main.
+    :returns: TODO
+
+    """
+
+    cv2.namedWindow("Detection", cv2.WINDOW_AUTOSIZE)
+
+    url = 'http://192.168.7.15'
+    clr = [136, 152, 235] # BGR
+    tlr = 50
+
+    clr_range = [
+        (clr[0]-tlr, clr[1]-tlr, clr[2]-tlr),
+        (clr[0]+tlr, clr[1]+tlr, clr[2]+tlr)
+    ]
+
+    user_in = input("Stream address ['" + url + "']:")
+    url     = url if not user_in else user_in
+
+    while True:
+        with urllib.request.urlopen(url + '/bmp') as response:
+            buf  = np.array(bytearray(response.read()), dtype=np.uint8)
+            img  = cv2.imdecode(buf, -1)
+            mask = cv2.inRange(img, clr_range[0], clr_range[1])
+
+            out = cv2.bitwise_and(img, img, mask=mask)
+            cv2.imshow("img", np.hstack([img, out]))
+            cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    main()
