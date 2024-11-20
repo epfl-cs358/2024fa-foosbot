3832dd4e4ec7d0cd14c20c73fd3942646add4689
Robin Nicole <robin.nicole@epfl.ch>
2024-11-20 18:31:40 +0100

    Circle detection works (#21)

--- Software/Camera/transmit_image_test/python-wifi/main.py
+++ Software/Camera/transmit_image_test/python-wifi/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import urllib.request
 import cv2
 import numpy as np
@@ -9,8 +10,6 @@ def main():
     :returns: TODO
 
     """
-
-    cv2.namedWindow("Detection", cv2.WINDOW_AUTOSIZE)
 
     url = 'http://192.168.7.15'
     # #ae5757 is also a good fit
@@ -29,11 +28,35 @@ def main():
         with urllib.request.urlopen(url + '/bmp') as response:
             buf  = np.array(bytearray(response.read()), dtype=np.uint8)
             img  = cv2.imdecode(buf, -1)
-            mask = cv2.inRange(img, clr_range[0], clr_range[1])
 
-            out = cv2.bitwise_and(img, img, mask=mask)
-            cv2.imshow("img", np.hstack([img, out]))
-            cv2.waitKey(0)
+            grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            grey = cv2.medianBlur(grey, 5)
+
+            rows = grey.shape[0]
+
+            circles = cv2.HoughCircles(grey, cv2.HOUGH_GRADIENT, 1, rows / 8,
+                                       param1=100, param2=30,
+                                       minRadius=1, maxRadius=100)
+
+            if circles is not None:
+                circles = np.uint16(np.around(circles))
+                for i in circles[0, :]:
+                    center = (i[0], i[1])
+                    # circle center
+                    cv2.circle(img, center, 1, (0, 100, 100), 3)
+                    # circle outline
+                    radius = i[2]
+                    cv2.circle(img, center, radius, (255, 0, 0), 3)
+
+            # mask = cv2.inRange(img, clr_range[0], clr_range[1])
+            # out = cv2.bitwise_and(img, img, mask=mask)
+            # cv2.imshow("img", np.hstack([img, out]))
+
+            try:
+                cv2.imshow("Output", img)
+                cv2.waitKey(1)
+            except:
+                sys.exit(0)
 
 
 if __name__ == "__main__":
