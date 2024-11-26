49d14b560fe03322e1925d012f98d2d6252829c6
Robin Nicole <robin.nicole@epfl.ch>
2024-11-26 18:00:53 +0100

    Started parallelism

--- Software/Camera/transmit_image_test/python-wifi/main.py
+++ Software/Camera/transmit_image_test/python-wifi/main.py
@@ -3,12 +3,67 @@
 import sys
 import serial
 import cv2
+from multiprocessing import Pool
 import urllib.request as urllr
 import numpy as np
 
 MSG_START = ':'
 MSG_SEP   = ';'
 MSG_END   = '\r\n'
+
+def get_img():
+    """
+    Gets the image from the URL.
+    :returns: BMP image.
+
+    """
+    sent = urllr.urlopen(url + "/bmp")
+    buf  = np.array(bytearray(sent.read()), dtype=np.uint8)
+    return buf
+
+def process_img(buf: np.ndarray):
+    """
+    Processes the image for ball detection.
+
+    :buf: The buffer containing the image.
+    :returns: The position of the ball and the time stamp.
+
+    """
+    if buf.size == 0:
+        return (-1, (-1, -1), -1)
+    else:
+        print(buf)
+        img  = cv2.imdecode(buf, -1)
+
+        grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        grey = cv2.medianBlur(grey, 5)
+
+        rows = grey.shape[0]
+
+        circles = cv2.HoughCircles(grey, cv2.HOUGH_GRADIENT, 1, rows / 8,
+                                   param1=100, param2=30,
+                                   minRadius=1, maxRadius=100)
+
+        pos = (-1, -1)
+
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0, :]:
+                center = (i[0], i[1])
+                # circle center
+                cv2.circle(img, center, 1, (0, 100, 100), 3)
+                # circle outline
+                radius = i[2]
+                print("Radius: " + str(radius))
+                cv2.circle(img, center, radius, (255, 0, 0), 3)
+
+        # mask = cv2.inRange(img, clr_range[0], clr_range[1])
+        # out = cv2.bitwise_and(img, img, mask=mask)
+        # cv2.imshow("img", np.hstack([img, out]))
+
+            pos = (circles[0, 0][0], circles[0, 0][1])
+        timestmp = 0 # TODO
+        return (pos, timestmp, img)
 
 def main(url = 'http://192.168.7.15', urlMan=False, debug=False):
     """
@@ -51,52 +106,18 @@ def main(url = 'http://192.168.7.15', urlMan=False, debug=False):
         ser.open()
         print("Serial opened.")
 
-    # Create a window for Displaying images in real-time
-    cv2.namedWindow("Output")
+    pool = Pool()
 
     # Main Loop for image processing
     try:
         while True:
-            sent = urllr.urlopen(url + "/bmp")
-            buf = np.array(bytearray(sent.read()), dtype=np.uint8)
-            if buf.size == 0:
-                pass
+            buf = pool.apply_async(get_img, [])
+            (pos, timestmp, img) = pool.apply_async(process_img, [buf])
+
+            if debug:
+                print(pos)
             else:
-                print(buf)
-                img  = cv2.imdecode(buf, -1)
-
-                grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                grey = cv2.medianBlur(grey, 5)
-
-                rows = grey.shape[0]
-
-                circles = cv2.HoughCircles(grey, cv2.HOUGH_GRADIENT, 1, rows / 8,
-                                           param1=100, param2=30,
-                                           minRadius=1, maxRadius=100)
-
-                pos = (-1, -1)
-
-                if circles is not None:
-                    circles = np.uint16(np.around(circles))
-                    for i in circles[0, :]:
-                        center = (i[0], i[1])
-                        # circle center
-                        cv2.circle(img, center, 1, (0, 100, 100), 3)
-                        # circle outline
-                        radius = i[2]
-                        print("Radius: " + str(radius))
-                        cv2.circle(img, center, radius, (255, 0, 0), 3)
-
-                # mask = cv2.inRange(img, clr_range[0], clr_range[1])
-                # out = cv2.bitwise_and(img, img, mask=mask)
-                # cv2.imshow("img", np.hstack([img, out]))
-
-                    pos      = (circles[0, 0][0], circles[0, 0][1])
-                timestmp = 0 # TODO
-
-                if debug:
-                    print(pos)
-                else:
+                if pos[0] != -1 and pos[1] != -1:
                     ser.write(MSG_START + str(pos) + MSG_SEP + str(timestmp) + MSG_END)
 
                 cv2.imshow("Output", img)
