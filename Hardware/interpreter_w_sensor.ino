8300d07205eb5b6cb09a4a2d0971eac6ce8d42e1
Nur Ertug <115902349+NurErtugn@users.noreply.github.com>
2024-12-15 18:22:13 +0100

    Update interpreter_w_sensor.ino

--- Hardware/interpreter_w_sensor.ino
+++ Hardware/interpreter_w_sensor.ino
@@ -5,25 +5,33 @@
 #define Y_STP 3
 #define Z_DIR 7
 #define Z_STP 4
-#define sY_front 9
-#define sY_back 12
-#define sX_front 2 //POS VALUE
-#define sX_back 5 // NEG VALUE 
+#define A_STP 13
+#define A_DIR 9
+#define X_DIR 5
+#define X_STP 2
+
+
+// Sensor Pins
+#define sY_front 10 // Front endstop for Y-axis
+#define sY_back 11  // Back endstop for Y-axis
+#define sZ_front 12 // Front endstop for Z-axis
+#define sZ_back A0  // Back endstop for Z-axis
 
 // 1000 units is for the full range side to side
 const float stepsPerMM = 400.0; // Adjust based on your setup ==> nb of steps required to move 1mm
-SoftwareSerial wemosSerial1(-1, 10);  // single player pole rotary
- // double player pole rotary
-SoftwareSerial wemosSerial2(-1, 11);  // single player pole rotary
+
 
 // AccelStepper set up this is for the side to side of rod 1 (goalie)
 AccelStepper stepperY(1,Y_STP, Y_DIR);
 // other side to side road
-AccelStepper stepperX(1,Z_STP, Z_DIR);
+AccelStepper stepperZ(1,Z_STP, Z_DIR); 
+// rotation motor for stepperY
+AccelStepper stepperX(1,X_STP, X_DIR);
+// rotation motor for stepperX
+AccelStepper stepperA(1,A_STP, A_DIR);
 
-long TravelY;
 
-const float stepsPerRevolution = 3200.0; 
+const float stepsPerRevolution =2000.0; 
 const float degreesPerStep = 360.0 / stepsPerRevolution; 
 int acceleration = 5;   
 
@@ -33,16 +41,16 @@ void setBeginning(){
   // max is 1000 units
   int full_distance = 1000;
   moveSide(stepperX,sY_front, sY_back,full_distance); // Move in the positive direction, going backwards
-  moveSide(stepperY,sX_front, sX_back, full_distance);
+  moveSide(stepperZ,sZ_front, sZ_back, full_distance);
   // for both stops when the sensor is triggered 
   int middlePosition = -full_distance/2 ;
   
-  rotateByAngle(wemosSerial1,0 ); 
-  rotateByAngle(wemosSerial2,0 );  // TODO: check if it works  ==> should be bc encoder understands current angle that it is on 
+  rotateByAngle(stepperX,0 ); 
+  rotateByAngle(stepperA,0 );  // TODO: check if it works  ==> should be bc encoder understands current angle that it is on 
   moveSide(stepperY,sY_front, sY_back,middlePosition);
   stepperY.setCurrentPosition(middlePosition); // now this is 0 position
 
-  moveSide(stepperX,sX_front, sX_back,middlePosition);
+  moveSide(stepperZ,sZ_front, sZ_back,middlePosition);
   stepperX.setCurrentPosition(middlePosition); // now this is 0 position
   
 }
@@ -71,12 +79,32 @@ void moveSide(AccelStepper &stepper,int sensor1, int sensor2,  int value){
   Serial.println(value);
 }
 
+void moveSide2(AccelStepper &stepper, int value){
+  int y =  stepper.currentPosition(); 
+  stepper.move(value);          // Set the final position 
+  //for coordinate value thats been going far from the side motor >0, controlled by the pin 11
+  if(value>0){
+    while (stepper.distanceToGo() != 0  ) {
+      stepper.run();                  // Continuously move toward the target
+    }
+    stepper.stop();
+  }
+  else {
+    while (stepper.distanceToGo() != 0  ) {
+      stepper.run();                  // Continuously move toward the target
+    }
+    stepper.stop();
+  }
+  Serial.print("Moved by ");
+  Serial.println(value);
+}
+
 //INITIALX
 void returnToInitialPositionSide() {
   stepperY.moveTo(0);            // Command to move to position 0
-  stepperX.moveTo(0);
+  stepperZ.moveTo(0);
   stepperY.runToPosition();      // Execute the movement
-  stepperX.runToPosition();
+  stepperZ.runToPosition();
   Serial.print("Returned to initial X position ");
 }
 
@@ -84,13 +112,8 @@ void returnToInitialPositionSide() {
 // <0 value moves forward 
 // >0 moves bavkwards
 // angle = +-12 == 180 deg rotation
-void rotateByAngle(SoftwareSerial &serialPort, float angle) {
-  float rad_angle = angle * 3.14/180;
-  // MOVE <angle>
-  String command = "MOVE " + String(rad_angle);
-  serialPort.println(command);
-  Serial.print("Rotated by ");
-  Serial.println(angle);
+void rotateByAngle(AccelStepper &stepper, int angle) {
+  moveSide2(stepper,angle);
 }
 //
 
@@ -102,15 +125,11 @@ void executeInterpreter(String command) {
   if (strcmp(cmd, "MOVE1") == 0) {
     moveSide(stepperY, sY_front,sY_back, value);
   }else if(strcmp(cmd, "MOVE2")==0){
-    moveSide(stepperX, sX_front, sX_back,value);
+    moveSide(stepperZ, sZ_front, sZ_back,value);
   } else if (strcmp(cmd, "ROTATE1") == 0) {
-    rotateByAngle(wemosSerial1,(float)value); // Rotate by angle single player pole
+    moveSide2(stepperX,value); // Rotate by angle single player pole
   }else if(strcmp(cmd, "ROTATE2") == 0){
-    rotateByAngle(wemosSerial2,(float)value); // Rotate by angle double player pole
-  } else if (strcmp(cmd, "INITIALX") == 0) {
-    returnToInitialPositionSide();
-  } else if(strcmp(cmd, "INITIALY")== 0){
-    rotateByAngle(wemosSerial1, 0);
+    moveSide2(stepperA,value); // Rotate by angle double player pole
   }else if(strcmp(cmd, "BEGIN")==0){
     setBeginning();
   } else {
@@ -122,11 +141,14 @@ void executeInterpreter(String command) {
 void setup()
 {
   Serial.begin(9600); 
-  wemosSerial1.begin(9600);
   stepperY.setMaxSpeed(5000.0); // set max speed of the stepper , slower to get better accuracy
   stepperY.setAcceleration(5000.0); //set acceleration of the stepper 
   stepperX.setMaxSpeed(5000.0); // set max speed of the stepper , slower to get better accuracy
   stepperX.setAcceleration(5000.0); //set acceleration of the stepper 
+  stepperZ.setMaxSpeed(5000.0); // set max speed of the stepper , slower to get better accuracy
+  stepperZ.setAcceleration(5000.0); //set acceleration of the stepper 
+  stepperA.setMaxSpeed(5000.0); // set max speed of the stepper , slower to get better accuracy
+  stepperA.setAcceleration(5000.0); //set acceleration of the stepper 
   pinMode(EN, OUTPUT); 
   digitalWrite(EN, LOW); // Enable motor driver
   //stepperY.setCurrentPosition(0);  // initialize the current position im at to be 0 
