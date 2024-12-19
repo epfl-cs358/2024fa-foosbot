8ebbd9077959f15a4257d12f6f15e3de05ba742f
Robin Nicole <robin.nicole@epfl.ch>
2024-12-19 11:30:17 +0100

    Document and clean up code

--- Software/Algorithms/debug/library/ParallelInterpreter.h
+++ Software/Algorithms/debug/library/ParallelInterpreter.h
@@ -5,26 +5,58 @@
 #include <SoftwareSerial.h>
 #include <Arduino.h>
 
+
 class ParallelIntepreter {
   public:
-    ParallelIntepreter(int yDir, int yStep, int zDir, int zStep, int aDir, int aStep, int xDir, int xStep, int enPin, int sY_front, int sY_back, int sZ_front, int sZ_back);
-    
+    ParallelIntepreter(int yDir, int yStep,
+                       int zDir, int zStep,
+                       int aDir, int aStep,
+                       int xDir, int xStep,
+                       int enPin,
+                       int sY_front, int sY_back,
+                       int sZ_front, int sZ_back,
+                       int fieldSize);
+
+    /*
+     * TODO: Doc
+     *
+     * @param value 
+     *
+     * @returns 
+     */
     int  intLength(int value);
+
+    /*
+     * Set stepper's target if every safety check has been verified.
+     *
+     * @param cmd   Command to be executed
+     * @param value Value of movement
+     */
     void setTarget(const char* cmd, int value);
+
+    /*
+     * TODO: Doc
+     */
     void rangeEstimation();
+
+    /*
+     * Parses the given input and executes the resulting command.
+     *
+     * @param command Command to be executed
+     */
     void executeInterpreter(String command);
+
+    /*
+     * Controls motors without blocking and checks for sensor input for
+     * safety check.
+     */
     void moveMotorsWithSensors();
+
+    /*
+     * TODO: Doc
+     */
     void setupSteppers();
 
-
-    void setBeginning();
-    void moveSide(AccelStepper &stepper, int sensor1, int sensor2, double value);
-    void moveSide2(AccelStepper &stepper, double value);
-    void returnToInitialPositionSide();
-    void rotateByAngle(AccelStepper &stepper, double angle);
-    void executeInterpreter(String command);
-    void setupSteppers();
-  
   private:
     AccelStepper stepperY;
     AccelStepper stepperZ;
@@ -33,7 +65,7 @@ class ParallelIntepreter {
     int EN;
     int Y_DIR, Y_STP, Z_DIR, Z_STP, A_DIR, A_STP, X_DIR, X_STP;
     int sY_front, sY_back, sZ_front, sZ_back;
-    int fieldSize;
+    int FIELD_SIZE;
 };
 
 #endif //PARALLEL_INTERPRETER_H
